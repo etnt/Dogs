18abccc65310f34440987e8c6c261f148a8c91ef
Torbjörn Törnkvist <kruskakli@gmail.com>
2024-02-21 06:08:05 +0100

    Adding Surface top composable to get Dark/Light theme to work

--- app/src/main/java/se/kruskakli/dogs/domain/MainActivity.kt
+++ app/src/main/java/se/kruskakli/dogs/domain/MainActivity.kt
@@ -3,6 +3,10 @@ package se.kruskakli.dogs.domain
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import se.kruskakli.dogs.network.KtorClient
 import se.kruskakli.dogs.ui.theme.DogsTheme
@@ -14,7 +18,12 @@ class MainActivity : ComponentActivity() {
             val viewModel: BreedViewModel = viewModel()
             viewModel.fetchRandomBreed()
             DogsTheme {
-                Navigation(viewModel)
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Navigation(viewModel)
+                }
             }
         }
     }
